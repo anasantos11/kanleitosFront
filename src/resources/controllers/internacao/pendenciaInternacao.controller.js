24ad2d0774b164e97a4f4f79496ea933edd035fb
Ana <anasantos1103@gmail.com>
2018-05-06 11:12:38 -0300

    Realizado conversão das datas das pendências para formato de data

--- src/resources/controllers/internacao/pendenciaInternacao.controller.js
+++ src/resources/controllers/internacao/pendenciaInternacao.controller.js
@@ -1,10 +1,21 @@
-app.controller('PendenciaInternacao', ["$scope", "$http", "svcPendenciaInternacao", "svcTipoPendencia", "Notify",
-    function ($scope, $http, svcPendenciaInternacao, svcTipoPendencia, Notify) {
+app.controller('PendenciaInternacao', ["$scope", "$http", "svcPendenciaInternacao", "svcTipoPendencia", "Notify", "$filter",
+    function ($scope, $http, svcPendenciaInternacao, svcTipoPendencia, Notify, $filter) {
 
         if (!isNullOrEmpty($scope.ngDialogData.idRegistroInternacao)) {
             svcPendenciaInternacao.getPendenciasInternacao($scope.ngDialogData.idRegistroInternacao)
                 .then(function (res) {
                     $scope.listaPendencias = res.data.data;
+                    $scope.listaPendencias.forEach(function(pendencia) {
+                        if(!isNullOrEmpty(pendencia.dataInicio))
+                            pendencia.dataInicio = new Date(pendencia.dataInicio);
+                        
+                        if(!isNullOrEmpty(pendencia.previsaoConclusao))
+                            pendencia.previsaoConclusao = new Date(pendencia.previsaoConclusao);
+                        
+                        if(!isNullOrEmpty(pendencia.dataConclusao))
+                            pendencia.dataConclusao = new Date(pendencia.dataConclusao);
+                    });
+                    
                 })
         };
 
