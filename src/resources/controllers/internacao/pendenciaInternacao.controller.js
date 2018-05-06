5cfd681dea90dcebe3df87574bc3aac74d0334d6
Ana <anasantos1103@gmail.com>
2018-05-06 12:24:51 -0300

    Adicionado funcionalidade para atualizar pendência da internação  / Incluso operação para cancelar edição da pendência de internaçõa

--- src/resources/controllers/internacao/pendenciaInternacao.controller.js
+++ src/resources/controllers/internacao/pendenciaInternacao.controller.js
@@ -5,17 +5,17 @@ app.controller('PendenciaInternacao', ["$scope", "$http", "svcPendenciaInternaca
             svcPendenciaInternacao.getPendenciasInternacao($scope.ngDialogData.idRegistroInternacao)
                 .then(function (res) {
                     $scope.listaPendencias = res.data.data;
-                    $scope.listaPendencias.forEach(function(pendencia) {
-                        if(!isNullOrEmpty(pendencia.dataInicio))
+                    $scope.listaPendencias.forEach(function (pendencia) {
+                        if (!isNullOrEmpty(pendencia.dataInicio))
                             pendencia.dataInicio = new Date(pendencia.dataInicio);
-                        
-                        if(!isNullOrEmpty(pendencia.previsaoConclusao))
+
+                        if (!isNullOrEmpty(pendencia.previsaoConclusao))
                             pendencia.previsaoConclusao = new Date(pendencia.previsaoConclusao);
-                        
-                        if(!isNullOrEmpty(pendencia.dataConclusao))
+
+                        if (!isNullOrEmpty(pendencia.dataConclusao))
                             pendencia.dataConclusao = new Date(pendencia.dataConclusao);
                     });
-                    
+
                 })
         };
 
@@ -24,8 +24,27 @@ app.controller('PendenciaInternacao', ["$scope", "$http", "svcPendenciaInternaca
                 $scope.tiposPendencias = res.data.data;
             });
 
-       $scope.updatePendencia = function(pendencia){
-            debugger;
+        $scope.updatePendenciaInternacao = function (pendencia) {
+            svcPendenciaInternacao.updatePendenciaInternacao(pendencia)
+                .then(function (res) {
+                    alertaSucesso("Pendência de Internação atualizada com sucesso.")
+                    pendencia.Edicao = false;
+                    delete $scope.copiaListaPendencias;
+                })
+                .catch(function (err) {
+                    alertaErroRequisicao(err);
+                })
         };
+
+        $scope.editarPendenciaInternacao = function (pendencia) {
+            $scope.copiaListaPendencias = angular.copy($scope.listaPendencias);
+            pendencia.Edicao = true;
+        };
+
+        $scope.cancelarEdicao = function (pendencia) {
+            $scope.listaPendencias = $scope.copiaListaPendencias;
+            pendencia.Edicao = false;
+        };
+
     }
 ]);
