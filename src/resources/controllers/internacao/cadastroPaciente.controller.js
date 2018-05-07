a40f79a3a6b4c9607aff4eaeff56882fa7eac711
Ana <anasantos1103@gmail.com>
2018-05-07 20:04:21 -0300

    Alterado alertas do cadastro de paciente para usar função genérica.

--- src/resources/controllers/internacao/cadastroPaciente.controller.js
+++ src/resources/controllers/internacao/cadastroPaciente.controller.js
@@ -1,119 +1,71 @@
 app.controller('ModalCadastroController', ["$scope", "$http", "$filter", "pacienteFactory", "Notify",
-function ($scope, $http, $filter, pacienteFactory, Notify) {
+    function ($scope, $http, $filter, pacienteFactory, Notify) {
 
-    $scope.paciente = {
-        numProntuario: null,
-        nomePaciente: null,
-        nomeMae: null,
-        dataNascimento: null,
-        idade: null,
-        genero: null,
-    }
+        $scope.paciente = {
+            numProntuario: null,
+            nomePaciente: null,
+            nomeMae: null,
+            dataNascimento: null,
+            idade: null,
+            genero: null,
+        }
 
-    $scope.cadastrarPaciente = function (paciente) {
-        if ($scope.validarDadosPaciente()) {
-            $scope.paciente.dataNascimento = moment($scope.paciente.dataNascimento).format();
-            pacienteFactory.savePaciente($scope.paciente)
-                .then(function (response) {
-                    if(response.data.data > 0){                    
-                        swal( 'Concluído!',
-                        'Cadastro realizado com sucesso',
-                        'success'
-                        )
+        $scope.cadastrarPaciente = function (paciente) {
+            if ($scope.validarDadosPaciente()) {
+                $scope.paciente.dataNascimento = moment($scope.paciente.dataNascimento).format();
+                pacienteFactory.savePaciente($scope.paciente)
+                    .then(function (response) {
+                        alertaSucesso("Cadastro realizado com sucesso.");
                         return $scope.closeThisDialog($scope.paciente);
-                    }else{
-                        swal( 'Erro!',
-                            'Paciente já cadastrado!',
-                            'error'
-                        )
-                        return;
-                    }
-                    
-                })
-                .catch((response)=>{
-                        if (response.data != undefined) {
-                            swal(
-                                'Erro!',
-                                response.data.messages,
-                                'error'
-                            )
-                        } else {
-                            swal(
-                                'Erro!',
-                                response.message,
-                                'error'
-                            )
-                        }
                     })
-                }
+                    .catch(function (err) {
+                        alertaErroRequisicao(err);
+                    })
+            }
+        }
+
+        $scope.validarDadosPaciente = function () {
+            if (!$scope.paciente.numProntuario) {
+                alertaPreenchimentoCampo("número do prontuário");
+                return;
+            }
+            if (!$scope.paciente.nomePaciente) {
+                alertaPreenchimentoCampo("nome do paciente");
+                return;
+            }
+            if (!$scope.paciente.nomeMae) {
+                alertaPreenchimentoCampo("nome da mãe do paciente");
+                return;
+            }
+            if (!$scope.paciente.dataNascimento) {
+                alertaPreenchimentoCampo("data de nascimento");
+                return;
+            }
+            if (!$scope.paciente.idade) {
+                alertaPreenchimentoCampo("idade");
+                return;
+            }
+            if (!$scope.paciente.genero) {
+                alertaPreenchimentoCampo("sexo");
+                return;
+            }
+            return true;
+        }
+
+        $scope.calcularIdade = function () {
+
+            // Obtém a idade em milissegundos
+            var idadeP = new Date() - new Date($scope.paciente.dataNascimento).getTime();
+
+            // Converte os milissegundos em data e subtrai da era linux
+            var idadeData = new Date(idadeP);
+            var idade = idadeData.getUTCFullYear() - 1970;
+            if (!isNaN(idade) && idade != undefined) {
+                $scope.paciente.idade = idade;
+            } else {
+                $scope.paciente.idade = 0;
+            }
+
+        }
     }
-
-    $scope.validarDadosPaciente = function () {
-        if (!$scope.paciente.numProntuario) {
-            swal(
-                'Erro!',
-                'Digite o número do prontuário!',
-                'error'
-            )
-            return;
-        }
-        if (!$scope.paciente.nomePaciente) {
-            swal(
-                'Erro!',
-                'Digite o nome do paciente!',
-                'error'
-            )
-            return;
-        }
-        if (!$scope.paciente.nomeMae) {
-            swal(
-                'Erro!',
-                'Digite o nome da mãe do paciente!',
-                'error'
-            )
-            return;
-        }
-        if (!$scope.paciente.dataNascimento) {
-            swal(
-                'Erro!',
-                'Digite a data de nascimento!',
-                'error'
-            )
-            return;
-        }
-        if (!$scope.paciente.idade) {
-            swal(
-                'Erro!',
-                'Digite a idade do paciente!',
-                'error'
-            )
-            return;
-        }
-        if (!$scope.paciente.genero) {
-            swal(
-                'Erro!',
-                'Digite o sexo do paciente!',
-                'error'
-            )
-            return;
-        }
-        return true;
-    }
-
-    $scope.calcularIdade = function () {
-
-        // Obtém a idade em milissegundos
-        var idadeP = new Date() - new Date($scope.paciente.dataNascimento).getTime();
-
-        // Converte os milissegundos em data e subtrai da era linux
-        var idadeData = new Date(idadeP);
-        var idade = idadeData.getUTCFullYear() - 1970;
-        if (!isNaN(idade) && idade != undefined) {
-            $scope.paciente.idade = idade;
-        } else {
-            $scope.paciente.idade = 0;
-        }
-
-    }
-}
 ]);
