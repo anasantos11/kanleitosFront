49cb3f9f40c1f518c7b75d50150a37f94783acb9
Ana <anasantos1103@gmail.com>
2018-06-10 17:31:18 -0300

    Ajustado diretiva de registro de internações para que ela carregue e atualize os registros de internações

--- src/resources/directives/registrosInternacao.directive.js
+++ src/resources/directives/registrosInternacao.directive.js
@@ -1,34 +1,84 @@
-app.directive('listaRegistrosInternacao', function (Notify) {
-    return {
-        templateUrl: "templates/directives/registrosInternacao.html",
-        scope: {
-            registros: "=",
-            ocultaLegenda: "=",
-            showAcoes: "=",
-            dadosFiltros: "=",
-            evento: "=",
-            ocultaStatus: "=ocultaStatus",
-        },
-        link: function (scope, element, attrs) {
+app.directive('listaRegistrosInternacao', function (registroInternacaoFactory, $filter, $rootScope, Notify) {
+	return {
+		templateUrl: "templates/directives/registrosInternacao.html",
+		scope: {
+			registros: "=",
+			ocultaLegenda: "=",
+			showAcoes: "=",
+			dadosFiltros: "=",
+			evento: "=",
+			ocultaStatus: "=ocultaStatus"
+		},
+		link: function (scope, element, attrs) {
 
-            scope.closeModal = function (registro) {
-                scope.$parent.closeThisDialog(registro);
-            }
+			scope.evento = "filtrarRegistros";
+			scope.novoFiltro = function () {
+				scope.dadosFiltros = {
+					idAla: "",
+					idEnfermaria: "",
+					idLeito: "",
+					medicoResponsavel: "",
+					residenteResponsavel: "",
+					idIsolamento: "",
+					nomePaciente: "",
+					numProntuario: null,
+					dataAdmissao: null,
+					previsaoAlta: null,
+					classificacao: "",
+					status: "Em Andamento"
+				};
+			};
 
-            scope.openModalPendenciasInternacao = function (registro) {
-                return Notify.openModal("templates/internacao/pendenciaInternacao.html", { idRegistroInternacao: registro.idRegistroInternacao }, "95%")
-            };
+			scope.atualizaPacientesInternados = function () {
+				var dataAtual = new Date();
+				for (var i = 0; i < scope.registros.length; i++) {
+					var diasInternado = calcularDiasDiferenca(dataAtual, new Date(scope.registros[i].pedidoInternacao.dataAdmissao)).dias;
+					if (diasInternado < 6) {
+						scope.registros[i].classificacao = "verde";
+						scope.registros[i].diasInternado = diasInternado;
+					} else if (diasInternado < 11) {
+						scope.registros[i].classificacao = "amarelo";
+						scope.registros[i].diasInternado = diasInternado;
+					} else {
+						scope.registros[i].classificacao = "vermelho";
+						scope.registros[i].diasInternado = diasInternado;
+					}
+				}
+			};
 
-            scope.openModalObservacoesInternacao = function (registro) {
-                return Notify.openModal("templates/internacao/observacaoInternacao.html", { idRegistroInternacao: registro.idRegistroInternacao }, "95%")
-            };
+			scope.carregarRegistros = function () {
+				registroInternacaoFactory.getRegistrosInternacoes(scope.dadosFiltros)
+					.then(function (res) {
+						scope.registros = res.data.data;
+						scope.atualizaPacientesInternados();
+					})
+					.catch(function (res) {
+						console.log(res.data);
+					});
+			};
 
-            scope.openModalTransferenciasLeito = function (registro) {
-                return Notify.openModal("templates/internacao/transferenciaLeito.html", { registro: registro }, "95%")
-                .then(function(res){
-                    
-                })
-            };
-        }
-    };
+			$rootScope.$on(scope.evento, function (event) {
+				scope.carregarRegistros();
+			});
+
+			scope.openModalPendenciasInternacao = function (registro) {
+				return Notify.openModal("templates/internacao/pendenciaInternacao.html", { idRegistroInternacao: registro.idRegistroInternacao }, "95%")
+			};
+
+			scope.openModalObservacoesInternacao = function (registro) {
+				return Notify.openModal("templates/internacao/observacaoInternacao.html", { idRegistroInternacao: registro.idRegistroInternacao }, "95%")
+			};
+
+			scope.openModalTransferenciasLeito = function (registro) {
+				return Notify.openModal("templates/internacao/transferenciaLeito.html", { registro: registro }, "95%")
+			};
+
+			scope.closeModal = function (registro) {
+				scope.$parent.closeThisDialog(registro);
+			};
+
+			scope.novoFiltro();
+			scope.carregarRegistros();
+		}
+	};
 });
